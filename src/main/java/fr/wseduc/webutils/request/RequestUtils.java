57afcb0635949973c4559ec1b369d59c7397cbca
dboissin <damien.boissin@web-education.net>
2014-07-15 00:00:12 +0200

    Json schema validator

--- src/main/java/fr/wseduc/webutils/request/RequestUtils.java
+++ src/main/java/fr/wseduc/webutils/request/RequestUtils.java
@@ -1,11 +1,22 @@
 package fr.wseduc.webutils.request;
 
+import fr.wseduc.webutils.http.Renders;
+import fr.wseduc.webutils.validation.JsonSchemaValidator;
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.AsyncResultHandler;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.logging.Logger;
+import org.vertx.java.core.logging.impl.LoggerFactory;
 
 public class RequestUtils {
+
+	private static final Logger log = LoggerFactory.getLogger(RequestUtils.class);
+	private static final JsonSchemaValidator validator = JsonSchemaValidator.getInstance();
 
 	public static void bodyToJson(final HttpServerRequest request, final Handler<JsonObject> handler) {
 		request.bodyHandler(new Handler<Buffer>() {
@@ -15,7 +26,41 @@ public class RequestUtils {
 					JsonObject json = new JsonObject(event.toString("UTF-8"));
 					handler.handle(json);
 				} catch (RuntimeException e) {
-					request.response().setStatusCode(400).end(e.getMessage());
+					log.warn(e.getMessage(), e);
+					Renders.badRequest(request, e.getMessage());
+				}
+			}
+		});
+	}
+
+	public static void bodyToJson(final HttpServerRequest request, final String schema,
+			final Handler<JsonObject> handler) {
+		request.bodyHandler(new Handler<Buffer>() {
+			@Override
+			public void handle(Buffer event) {
+				try {
+					final JsonObject json = new JsonObject(event.toString("UTF-8"));
+					validator.validate(schema, json, new AsyncResultHandler<Message<JsonObject>>() {
+						@Override
+						public void handle(AsyncResult<Message<JsonObject>> event) {
+							if (event.succeeded()) {
+								if ("ok".equals(event.result().body().getString("status"))) {
+									handler.handle(json);
+								} else {
+									log.debug(event.result().body().getString("message"));
+									log.debug(event.result().body()
+											.getArray("report", new JsonArray()).encodePrettily());
+									Renders.badRequest(request, event.result().body().getString("error"));
+								}
+							} else {
+								log.error("Validate async error.", event.cause());
+								Renders.badRequest(request, event.cause().getMessage());
+							}
+						}
+					});
+				} catch (RuntimeException e) {
+					log.warn(e.getMessage(), e);
+					Renders.badRequest(request, e.getMessage());
 				}
 			}
 		});
---
+++ src/main/java/fr/wseduc/webutils/validation/JsonSchemaValidator.java
@@ -0,0 +1,99 @@
+package fr.wseduc.webutils.validation;
+
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.AsyncResultHandler;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.file.FileSystem;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.logging.Logger;
+import org.vertx.java.core.logging.impl.LoggerFactory;
+
+import java.io.File;
+
+
+public class JsonSchemaValidator {
+
+	private static final String JSONSCHEMA_PATH = "jsonschema";
+	private String address;
+	private EventBus eb;
+	private static final Logger log = LoggerFactory.getLogger(JsonSchemaValidator.class);
+
+	private JsonSchemaValidator() {}
+
+	private static class JsonSchemaValidatorHolder {
+		private static final JsonSchemaValidator instance = new JsonSchemaValidator();
+	}
+
+	public static JsonSchemaValidator getInstance() {
+		return JsonSchemaValidatorHolder.instance;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setEventBus(EventBus eb) {
+		this.eb = eb;
+	}
+
+	public void loadJsonSchema(final String keyPrefix, Vertx vertx) {
+		final FileSystem fs = vertx.fileSystem();
+		fs.exists(JSONSCHEMA_PATH, new Handler<AsyncResult<Boolean>>() {
+			@Override
+			public void handle(AsyncResult<Boolean> event) {
+				if (event.failed() || Boolean.FALSE.equals(event.result())) {
+					log.debug("Json schema directory not found.");
+					return;
+				}
+				fs.readDir(JSONSCHEMA_PATH, new Handler<AsyncResult<String[]>>() {
+					@Override
+					public void handle(AsyncResult<String[]> event) {
+						if (event.succeeded()) {
+							for (final String path : event.result()) {
+								final String key = keyPrefix + path.substring(
+										path.lastIndexOf(File.separatorChar) + 1, path.lastIndexOf('.'));
+								fs.readFile(path, new Handler<AsyncResult<Buffer>>() {
+									@Override
+									public void handle(AsyncResult<Buffer> event) {
+										if (event.succeeded()) {
+											JsonObject j = new JsonObject()
+													.putString("action","addSchema")
+													.putString("key", key)
+													.putObject("jsonSchema",
+															new JsonObject(event.result().toString()));
+											eb.send(address, j, new Handler<Message<JsonObject>>() {
+												@Override
+												public void handle(Message<JsonObject> event) {
+													if (!"ok".equals(event.body().getString("status"))) {
+														log.error(event.body().getString("message"));
+													}
+												}
+											});
+										} else {
+											log.error("Error loading json schema : " + path, event.cause());
+										}
+									}
+								});
+							}
+						} else {
+							log.error("Error loading json schemas.", event.cause());
+						}
+					}
+				});
+			}
+		});
+	}
+
+	public void validate(String schema, JsonObject json, AsyncResultHandler<Message<JsonObject>> handler) {
+		JsonObject j = new JsonObject()
+				.putString("action", "validate")
+				.putString("key", schema)
+				.putObject("json", json);
+		eb.sendWithTimeout(address, j, 10000, handler);
+	}
+
+}
