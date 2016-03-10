641ceecfb2644087847cb28fd10e14029a1ac1a2
dboissin <damien.boissin@web-education.net>
2016-03-10 15:17:20 +0100

    add string utils

--- src/main/java/fr/wseduc/webutils/Utils.java
+++ src/main/java/fr/wseduc/webutils/Utils.java
@@ -172,4 +172,12 @@ public class Utils {
 		return a;
 	}
 
+	public static boolean isEmpty(String string) {
+		return string == null || string.isEmpty();
+	}
+
+	public static boolean isNotEmpty(String string) {
+		return !isEmpty(string);
+	}
+
 }
