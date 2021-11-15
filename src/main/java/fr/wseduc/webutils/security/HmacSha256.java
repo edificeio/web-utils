65514333f1d55656c199073c4e6dde3f4fe86ded
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2021-11-15 17:53:27 +0100

    feat(security): Add a HMAC SHA256 class

---
+++ src/main/java/fr/wseduc/webutils/security/HmacSha256.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright © WebServices pour l'Éducation, 2014
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.wseduc.webutils.security;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+
+public class HmacSha256 {
+
+	public static String sign(String content, String key)
+			throws NoSuchAlgorithmException, InvalidKeyException,
+			IllegalStateException, UnsupportedEncodingException {
+		SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), "HmacSHA256");
+		Mac mac = Mac.getInstance("HmacSHA256");
+		mac.init(signingKey);
+		byte[] signed = mac.doFinal(content.getBytes("UTF-8"));
+		return Base64.getEncoder().encodeToString(signed);
+	}
+
+}
