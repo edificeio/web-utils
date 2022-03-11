b9a9021127e43ae584bf82c1d0af7fdea14e0c33
richard.bunel@opendigitaleducation.com <richard.bunel@opendigitaleducation.com>
2022-03-11 15:44:36 +0100

    fix(XSS): reinforce XSS filter with: * adding a Unicode decoder to prevent Unicode characters bypassing Patterns * adding a Base64 decoder to detect malicious code inside Base64 tokens * adding HTML entities for comma, colon, semicolon and equals signs

--- src/main/java/fr/wseduc/webutils/security/XSSUtils.java
+++ src/main/java/fr/wseduc/webutils/security/XSSUtils.java
@@ -19,9 +19,15 @@ package fr.wseduc.webutils.security;
 import io.vertx.core.MultiMap;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Base64;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import org.apache.commons.text.translate.AggregateTranslator;
 import org.apache.commons.text.translate.CharSequenceTranslator;
 import org.apache.commons.text.translate.EntityArrays;
@@ -30,11 +36,21 @@ import org.apache.commons.text.translate.NumericEntityUnescaper;
 
 public final class XSSUtils {
 
+	private static final Logger log = LoggerFactory.getLogger(XSSUtils.class);
+
+	private static final Map<CharSequence, CharSequence> UNESCAPE_ADDITIONAL_ENTITIES = new HashMap<>();
+	static {
+		UNESCAPE_ADDITIONAL_ENTITIES.put("&equals;", "\u003D");
+		UNESCAPE_ADDITIONAL_ENTITIES.put("&colon;", "\u003A");
+		UNESCAPE_ADDITIONAL_ENTITIES.put("&semi;", "\u003B");
+		UNESCAPE_ADDITIONAL_ENTITIES.put("&comma;", "\u002C");
+	}
 	private static final CharSequenceTranslator UNESCAPE_HTMLENTITIES =
             new AggregateTranslator(
                     new LookupTranslator(EntityArrays.BASIC_UNESCAPE),
                     new LookupTranslator(EntityArrays.ISO8859_1_UNESCAPE),
                     new LookupTranslator(EntityArrays.HTML40_EXTENDED_UNESCAPE),
+                    new LookupTranslator(UNESCAPE_ADDITIONAL_ENTITIES),
                     new NumericEntityUnescaper(NumericEntityUnescaper.OPTION.semiColonOptional)
             );
 
@@ -49,12 +65,13 @@ public final class XSSUtils {
 			Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
 			Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
 			Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+			Pattern.compile("atob\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
 			Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE),
 			Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE),
 			Pattern.compile("on(click|context|mouse|dblclick|key|abort|error|before|hash|load|page|" +
 					"resize|scroll|unload|blur|change|focus|input|invalid|reset|search|select|submit|drag|drop|copy|cut|paste|" +
 					"after|before|can|end|duration|emptied|play|progress|seek|stall|suspend|time|volume|waiting|message|open|touch|" +
-					"online|offline|popstate|show|storage|toggle|wheel)(\\s*\\w*\\s*)=",
+					"online|offline|popstate|show|storage|toggle|wheel|animationstart|begin)(\\s*\\w*\\s*)=",
 					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
 	};
 
@@ -71,16 +88,54 @@ public final class XSSUtils {
 		return m;
 	}
 
+	private static final Pattern unicodePattern = Pattern.compile("\\\\u([0-9A-Fa-f]{4})");
+
+	private static String unescapeUnicode(String value) {
+		Matcher unicodeMatcher = unicodePattern.matcher(value);
+		while (unicodeMatcher.find()) {
+			try {
+				value = unicodeMatcher.replaceAll(String.valueOf((char)(Integer.parseInt(unicodeMatcher.group(1), 16))));
+			} catch (Exception e) {
+				log.error("[XSSUtils] Error in escaping Unicode value", e);
+			}
+		}
+		return value;
+	}
+
+	private static Pattern base64Pattern = Pattern.compile("base64\\s*,\\s*((?:(?:(?:(?:\\\\[frnt])| )*[A-Z0-9+/]){4})*(?:(?:(?:(?:\\\\[frnt]| ))*[A-Z0-9+/]){2}==|(?:(?:(?:\\\\[frnt]| ))*[A-Z0-9+/]){3}=)?)",
+			Pattern.CASE_INSENSITIVE);
+
+	private static String unescapeBase64(String value) {
+		Matcher base64Matcher = base64Pattern.matcher(value);
+		while (base64Matcher.find()) {
+			try {
+				String group = base64Matcher.group(1);
+				group = group.replaceAll("(?:\\\\[frnt])| ", ""); // Removing whitespace characters before decoding
+				String base64Token = new String(Base64.getDecoder().decode(group));
+				String tmp = stripXSS(base64Token);
+				if (base64Token.length() != tmp.length()) {
+					value = base64Matcher.replaceAll("");
+				}
+			} catch (Exception e) {
+				log.error("[XSSUtils] Error in escaping Base64 value", e);
+			}
+		}
+		return value;
+	}
+
 	public static String stripXSS(String value) {
 		if (value != null) {
 			//value = ESAPI.encoder().canonicalize(value);
 			value = value.replaceAll("\0", "");
-			String tmp = UNESCAPE_HTMLENTITIES.translate(value);
-			final int originalDecodedLength = tmp.length();
+			String tmp = unescapeUnicode(value);
+			tmp = UNESCAPE_HTMLENTITIES.translate(tmp);
+			final int lengthBeforeBase64 = tmp.length();
+			tmp = unescapeBase64(tmp);
+			final int lengthAfterBase64 = tmp.length();
 			for (Pattern scriptPattern : patterns){
 				tmp = scriptPattern.matcher(tmp).replaceAll("");
 			}
-			if (originalDecodedLength != tmp.length()) {
+			if (lengthBeforeBase64 != lengthAfterBase64 || lengthAfterBase64 != tmp.length()) {
 				value = stripXSS(tmp);
 			}
 		}
