59eab99edc2cefdecc9f21d572044b41a508973b
mariusestaque <marius.estaque@opendigitaleducation.com>
2023-08-22 10:36:58 +0200

    WB-2005 improving implementation

--- src/main/java/fr/wseduc/transformer/impl/ContentTransformerClientWithMetrics.java
+++ src/main/java/fr/wseduc/transformer/impl/ContentTransformerClientWithMetrics.java
@@ -4,6 +4,7 @@ import fr.wseduc.transformer.IContentTransformerClient;
 import fr.wseduc.transformer.to.ContentTransformerRequest;
 import fr.wseduc.transformer.to.ContentTransformerResponse;
 import io.vertx.core.Future;
+import org.apache.commons.lang3.NotImplementedException;
 
 /**
  * Monitored content transformer client
@@ -12,6 +13,6 @@ public class ContentTransformerClientWithMetrics implements IContentTransformerC
     @Override
     public Future<ContentTransformerResponse> transform(ContentTransformerRequest request) {
         // TODO mest : implement metrics monitoring over content transformer client in WB-2009
-        return Future.succeededFuture();
+        throw new NotImplementedException("Metrics monitoring will be implemented in WB-2009");
     }
 }
