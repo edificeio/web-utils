85bdffdf2a2a39bbfd47a3f02bd3549ba5b6d1ea
Junior Bernard <junior.bernard@opendigitaleducation.com>
2023-08-25 15:34:16 +0200

    feat: #WB-2009, add metrics to content transformer

--- src/main/java/fr/wseduc/transformer/ContentTransformerFactoryProvider.java
+++ src/main/java/fr/wseduc/transformer/ContentTransformerFactoryProvider.java
@@ -1,10 +1,13 @@
 package fr.wseduc.transformer;
 
+import fr.wseduc.transformer.impl.ContentTransformerClientMetricsRecorder;
 import fr.wseduc.transformer.impl.NoopContentTransformerFactory;
 import fr.wseduc.transformer.impl.MonitoredContentTransformerFactory;
 import fr.wseduc.transformer.impl.SimpleContentTransformerFactory;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 /**
  * Class providing implementations of {@link IContentTransformerFactory} upon context
@@ -12,6 +15,8 @@ import io.vertx.core.json.JsonObject;
 public class ContentTransformerFactoryProvider {
 
     private static Vertx vertx;
+
+    private static final Logger log = LoggerFactory.getLogger(ContentTransformerFactoryProvider.class);
 
     private ContentTransformerFactoryProvider() {};
 
@@ -28,19 +33,22 @@ public class ContentTransformerFactoryProvider {
      * @param contentTransformerConfig the content transformer client configuration
      * @return the content transformer client factory
      */
-    public static IContentTransformerFactory getFactory(JsonObject contentTransformerConfig) {
+    public static IContentTransformerFactory getFactory(final String appName, JsonObject contentTransformerConfig) {
         if(vertx == null ) {
             throw new IllegalArgumentException("content.transformer.factory.provider.missing.vertx");
         }
         final IContentTransformerFactory contentTransformerFactory;
         if (contentTransformerConfig == null) {
             contentTransformerFactory = NoopContentTransformerFactory.instance;
+            log.warn("No content transformer configured so rich content won't be synced");
         } else {
             final IContentTransformerFactory innerFactory;
             innerFactory = new SimpleContentTransformerFactory(vertx, contentTransformerConfig);
             final JsonObject metricsOptions = contentTransformerConfig.getJsonObject("content-transformer-metrics", new JsonObject());
             if (metricsOptions.getBoolean("enabled", false)) {
-                contentTransformerFactory = new MonitoredContentTransformerFactory(innerFactory);
+                contentTransformerFactory = new MonitoredContentTransformerFactory(
+                        innerFactory,
+                        ContentTransformerClientMetricsRecorder.Configuration.fromJson(appName, metricsOptions));
             } else {
                 contentTransformerFactory = innerFactory;
             }
