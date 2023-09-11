d1f545d65f8c2fb309aab4914a5ad9add92fab83
mariusestaque <marius.estaque@opendigitaleducation.com>
2023-09-11 15:15:34 +0200

    WB-2009 adding duration per byte metrics

--- src/main/java/fr/wseduc/transformer/IContentTransformerClientMetricsRecorder.java
+++ src/main/java/fr/wseduc/transformer/IContentTransformerClientMetricsRecorder.java
@@ -1,32 +1,32 @@
 package fr.wseduc.transformer;
 
-import fr.wseduc.transformer.to.ContentTransformerAction;
+import fr.wseduc.transformer.to.ContentTransformerRequest;
 
 public interface IContentTransformerClientMetricsRecorder {
     /**
      * Called when a successful transformation was performed.
-     * @param action Transformation performed
+     * @param request Transformation request
      * @param durationInMs Duration of the transformation
      */
-    void onTransformSuccess(final ContentTransformerAction action, final long durationInMs);
+    void onTransformSuccess(final ContentTransformerRequest request, final long durationInMs);
     /**
      * Called when a transformation raised an error.
-     * @param action Transformation performed
+     * @param request Transformation request
      * @param durationInMs Duration of the transformation
      */
-    void onTransformFailure(final ContentTransformerAction action, final long durationInMs);
+    void onTransformFailure(final ContentTransformerRequest request, final long durationInMs);
 
     static final NoopContentTransformerClientMetricsRecorder noop = new NoopContentTransformerClientMetricsRecorder();
 
     static class NoopContentTransformerClientMetricsRecorder implements IContentTransformerClientMetricsRecorder {
 
         @Override
-        public void onTransformSuccess(final ContentTransformerAction action, final long durationInMs) {
+        public void onTransformSuccess(final ContentTransformerRequest request, final long durationInMs) {
 
         }
 
         @Override
-        public void onTransformFailure(final ContentTransformerAction action, final long durationInMs) {
+        public void onTransformFailure(final ContentTransformerRequest request, final long durationInMs) {
 
         }
     }
