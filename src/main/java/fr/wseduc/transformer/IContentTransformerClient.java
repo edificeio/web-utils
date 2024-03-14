824e879978578cc595cdf46c31ee594a29727cec
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2024-03-14 11:24:40 +0100

    feat: #WB-2604, add Http request for the clients which could use them

--- src/main/java/fr/wseduc/transformer/IContentTransformerClient.java
+++ src/main/java/fr/wseduc/transformer/IContentTransformerClient.java
@@ -3,6 +3,7 @@ package fr.wseduc.transformer;
 import fr.wseduc.transformer.to.ContentTransformerRequest;
 import fr.wseduc.transformer.to.ContentTransformerResponse;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerRequest;
 
 /**
  * Client to call content transformer service
@@ -12,22 +13,25 @@ public interface IContentTransformerClient {
     /**
      * Dummy instance of content transformer client
      */
-    static final IContentTransformerClient noop = new NoopContentTransformerClient();
+    IContentTransformerClient noop = new NoopContentTransformerClient();
 
     /**
      * Method calling transformation service
      * @param request request specifying transformation method, content version and content to transform.
+     * @param httpCallerRequest incoming HTTP request that required a call to the transformer
      * @return response containing the transformed content
      */
-    Future<ContentTransformerResponse> transform(ContentTransformerRequest request);
+    Future<ContentTransformerResponse> transform(final ContentTransformerRequest request,
+                                                 final HttpServerRequest httpCallerRequest);
 
     /**
      * Dummy implementation of {@link IContentTransformerClient}
      */
-    public static class NoopContentTransformerClient implements IContentTransformerClient {
+    class NoopContentTransformerClient implements IContentTransformerClient {
 
         @Override
-        public Future<ContentTransformerResponse> transform(ContentTransformerRequest request) {
+        public Future<ContentTransformerResponse> transform(final ContentTransformerRequest request,
+                                                            final HttpServerRequest httpCallerRequest) {
             return Future.succeededFuture();
         }
     }
