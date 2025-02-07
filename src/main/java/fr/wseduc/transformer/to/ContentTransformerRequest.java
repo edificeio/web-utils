c726d0faad7c1c06fc5b4bdd4d305008d959bb7f
mariusode <125455829+mariusode@users.noreply.github.com>
2025-02-07 09:13:35 +0100

    feat: WB-3821, enabling content transformer client to request a set of app-specific additional content transformations (#33)
    feat: WB-3821, enabling content transformer client to request a set of app-specific additional content transformations

--- src/main/java/fr/wseduc/transformer/to/ContentTransformerRequest.java
+++ src/main/java/fr/wseduc/transformer/to/ContentTransformerRequest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.vertx.core.json.JsonObject;
 
+import java.util.Collections;
 import java.util.Set;
 
 /**
@@ -29,6 +30,10 @@ public class ContentTransformerRequest {
      * Json content to transform into html
      */
     private final JsonObject jsonContent;
+    /**
+     * Ids of additional extensions to be applied by the content transformer
+     */
+    private final Set<String> additionalExtensionIds;
 
     /**
      * Constructor
@@ -36,16 +41,30 @@ public class ContentTransformerRequest {
      * @param contentVersion version of content to transform
      * @param htmlContent html content to transform into json
      * @param jsonContent json content to transform into html
+     * @param additionalExtensionIds ids of additional extensions
      */
     @JsonCreator
     public ContentTransformerRequest(@JsonProperty("requestedFormats") Set<ContentTransformerFormat> requestedFormats,
                                      @JsonProperty("contentVersion") int contentVersion,
                                      @JsonProperty("htmlContent") String htmlContent,
-                                     @JsonProperty("jsonContent") JsonObject jsonContent) {
+                                     @JsonProperty("jsonContent") JsonObject jsonContent,
+                                     @JsonProperty("additionalExtensionIds") Set<String> additionalExtensionIds) {
         this.requestedFormats = requestedFormats;
         this.contentVersion = contentVersion;
         this.htmlContent = htmlContent;
-        this.jsonContent =jsonContent;
+        this.jsonContent = jsonContent;
+        this.additionalExtensionIds = additionalExtensionIds;
+    }
+
+    /**
+     * Constructor with empty additionalExtensionIds
+     * @param requestedFormats requested transformation formats
+     * @param contentVersion version of content to transform
+     * @param htmlContent html content to transform into json
+     * @param jsonContent json content to transform into html
+     */
+    public ContentTransformerRequest(Set<ContentTransformerFormat> requestedFormats, int contentVersion, String htmlContent, JsonObject jsonContent) {
+        this(requestedFormats, contentVersion, htmlContent, jsonContent, Collections.emptySet());
     }
 
     public Set<ContentTransformerFormat> getRequestedFormats() {
@@ -62,6 +81,10 @@ public class ContentTransformerRequest {
 
     public JsonObject getJsonContent() {
         return jsonContent;
+    }
+
+    public Set<String> getAdditionalExtensionIds() {
+        return additionalExtensionIds;
     }
 }
 
