02405fb48b00305b74fd5f01607dff18a3f08694
Vincent Billard <vincent.billard@edifice.io>
2025-09-19 10:28:54 +0200

    fix(template): #COCO-4672 refactor template processing to avoid race condition on lambda

--- src/main/java/fr/wseduc/webutils/template/TemplateProcessor.java
+++ src/main/java/fr/wseduc/webutils/template/TemplateProcessor.java
@@ -21,34 +21,24 @@ package fr.wseduc.webutils.template;
 
 import java.io.Writer;
 import java.io.StringWriter;
-import java.io.Reader;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 import com.samskivert.mustache.Mustache;
 import com.samskivert.mustache.Template;
 import fr.wseduc.webutils.collections.JsonUtils;
-import io.vertx.core.AsyncResult;
+import fr.wseduc.webutils.http.ProcessTemplateContext;
 import io.vertx.core.Handler;
-import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-
-import static fr.wseduc.webutils.data.FileResolver.absolutePath;
 
 public class TemplateProcessor
 {
   protected static final Logger log = LoggerFactory.getLogger(TemplateProcessor.class);
 
   protected Mustache.Compiler compiler = Mustache.compiler().defaultValue("");
-  private Map<String, Mustache.Lambda> templateLambdas = new ConcurrentHashMap<String, Mustache.Lambda>();
-
-  public TemplateProcessor()
-  {
-  }
+  private Map<String, Mustache.Lambda> templateLambdas = new ConcurrentHashMap<>();
 
   // =========================================== COMPILER CONFIGURATION ===========================================
 
@@ -66,12 +56,14 @@ public class TemplateProcessor
     return this;
   }
 
+  @Deprecated
   public TemplateProcessor defaultValue(String defaultValue)
   {
     this.compiler = this.compiler.defaultValue(defaultValue);
     return this;
   }
 
+  @Deprecated
   public TemplateProcessor escapeHTML(boolean enableHTMLEscaping)
   {
     this.compiler = this.compiler.escapeHTML(enableHTMLEscaping);
@@ -84,60 +76,13 @@ public class TemplateProcessor
   @Deprecated
   public void processTemplate(String templateString, JsonObject params, final Handler<String> handler)
   {
-    this.processTemplateToWriter(templateString, params, new Handler<Writer>()
-    {
-      @Override
-      public void handle(Writer w)
-      {
-        handler.handle(w == null ? null : w.toString());
-      }
-    });
-  }
-
-  public void processTemplate(String templateString, JsonObject params, Map<String, Mustache.Lambda> lambdas,
-                              final Handler<String> handler)
-  {
-    this.processTemplateToWriter(templateString, params, lambdas, new Handler<Writer>()
-    {
-      @Override
-      public void handle(Writer w)
-      {
-        handler.handle(w == null ? null : w.toString());
-      }
-    });
+    this.processTemplateToWriter(templateString, params, w -> handler.handle(w == null ? null : w.toString()));
   }
 
   @Deprecated
   public void processTemplateToWriter(String templateString, JsonObject params, final Handler<Writer> handler)
   {
-    this.getTemplate(templateString, new Handler<Template>()
-    {
-      @Override
-      public void handle(Template t)
-      {
-        processTemplate(t, params, handler);
-      }
-    });
-  }
-
-  public void processTemplateToWriter(String templateString, JsonObject params,  Map<String, Mustache.Lambda> lambdas,
-                                      final Handler<Writer> handler)
-  {
-    this.getTemplate(templateString, new Handler<Template>()
-    {
-      @Override
-      public void handle(Template t)
-      {
-        processTemplate(t, params, lambdas, handler);
-      }
-    });
-  }
-
-  protected void processTemplate(Template t, JsonObject params, Map<String, Mustache.Lambda> lambdas, final Handler<Writer> handler) {
-    final JsonObject ctxParams = (params == null) ? new JsonObject() : params.copy();
-    final Map<String, Object> ctx = JsonUtils.convertMap(ctxParams);
-    ctx.putAll(lambdas);
-    applyTemplate(t, ctx, handler);
+    this.getTemplate(templateString, t -> processTemplate(t, params, handler));
   }
 
   @Deprecated
@@ -149,13 +94,38 @@ public class TemplateProcessor
     applyTemplate(t, ctx, handler);
   }
 
-  protected void getTemplate(String templateString, final Handler<Template> handler)
-  {
+  @Deprecated
+  protected void getTemplate(String templateString, final Handler<Template> handler) {
     handler.handle(compiler.compile(templateString));
+  }
+
+  protected void getTemplate(ProcessTemplateContext context, final Handler<Template> handler) {
+    if(context.reader() == null) {
+      handler.handle(compiler.defaultValue(context.defaultValue())
+              .escapeHTML(context.escapeHtml())
+              .compile(context.templateString()));
+    } else {
+      handler.handle(compiler.defaultValue(context.defaultValue())
+              .escapeHTML(context.escapeHtml())
+              .compile(context.reader()));
+    }
+  }
+
+
+  public void processTemplate(ProcessTemplateContext templateContext, Template template, Handler<Writer> handler) {
+    final JsonObject ctxParams = (templateContext.params() == null) ? new JsonObject() : templateContext.params().copy();
+    final Map<String, Object> ctx = JsonUtils.convertMap(ctxParams);
+    applyLambdas(templateContext, ctx);
+    applyTemplate(template, ctx, handler);
+  }
+
+  public void processTemplate(ProcessTemplateContext templateContext, Handler<Writer> handler) {
+      getTemplate(templateContext, h -> processTemplate(templateContext, h, handler));
   }
 
 
   // ================================================ PRIVATE UTILS ===============================================
+
 
   private void applyTemplate(Template t, Map<String, Object> ctx, Handler<Writer> handler) {
     if (t != null) {
@@ -172,11 +142,13 @@ public class TemplateProcessor
     }
   }
 
-  private void applyLambdas(Map<String, Object> context)
-  {
-    for(Map.Entry<String, Mustache.Lambda> entry : this.templateLambdas.entrySet())
-    {
-      context.put(entry.getKey(), entry.getValue());
-    }
+  private void applyLambdas(ProcessTemplateContext templateContext, Map<String, Object> ctx) {
+      ctx.putAll(templateContext.lambdas());
   }
+
+  @Deprecated
+  private void applyLambdas(Map<String, Object> context) {
+      context.putAll(this.templateLambdas);
+  }
+
 }
