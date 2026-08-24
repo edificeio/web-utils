03c71cc277a1a6e99d1b5c76c7727391c7dbee05
Benjamin Perez <benjamin.perez@web-education.net>
2026-08-24 17:53:52 +0200

    fix(fileResolver): #ENABLING-1174 security fix

--- src/main/java/fr/wseduc/webutils/Server.java
+++ src/main/java/fr/wseduc/webutils/Server.java
@@ -181,6 +181,10 @@ public abstract class Server extends AbstractVerticle {
 		// Serve public static resource like img, css, js. By convention in /public directory
 		rm.getWithRegEx(prefix.replaceAll("\\/", "\\/") + "\\/public\\/.+", request -> {
 			final String path = absolutePath(request.path().substring(prefix.length() + 1));
+			if (path == null) { // folder crossing is forbidden
+				Renders.notFound(request);
+				return;
+			}
 			if (dev) {
 				request.response().sendFile(path, ar -> {
 					if (ar.failed() && !request.response().ended()) {
